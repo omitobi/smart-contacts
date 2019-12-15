0b7489b2a7fb846bfac3923ce3a71d0a7b61e9a7
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2019-12-15 23:03:49 +0200

    Add menu image icons to header bar

--- components/CoverHeader.js
+++ components/CoverHeader.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, Image} from "react-native";
 
 const CoverHeader = () => {
     return (
         <View style={styles.main}>
             <View style={styles.headerPlus}>
-                <Text>+</Text></View>
-            <View style={styles.headerText}>
-                <Text>My Profile</Text>
+                <Image source={require('./../assets/images/baseline_post_add_white_18dp.png')} />
             </View>
+            <Text style={styles.headerText}>
+                My Profile
+            </Text>
             <View style={styles.headerMenu}>
-                <Text>[]</Text>
+                <Image source={require('./../assets/images/baseline_post_add_white_18dp.png')} />
             </View>
         </View>
     )
@@ -23,12 +24,15 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
+        padding: 2,
     },
     headerPlus: {
         // flex: 1,
     },
     headerText: {
         textAlign: "center",
+        fontSize: 16,
+        color: "white"
     },
     headerMenu: {
         // flex: 1,
