ba0c0f0bed87aa3fc3bf766c705138e7cc8bdd8a
Oluwatobi Samuel Omisakin <omisakin.oluwatobi@gmail.com>
2019-12-18 23:45:25 +0200

    Load user contacts from phone

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, StatusBar, Text, View } from 'react-native';
 import Profile from "./components/Profile";
-import ProfileCover from "./components/ProfileCover";
-import ProfileBody from "./components/ProfileBody";
-import BaseNavBar from "./components/BaseNavBar";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
